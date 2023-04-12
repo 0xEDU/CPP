8ab163a18eca37c0de28c766a5f2f5eb0826fdd5
0xEDU <bfwaend@gmail.com>
2023-04-11 22:44:40 -0300

    feat: started ex02

---
+++ Module00/ex02/Account.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/11 22:07:44 by edu               #+#    #+#             */
+/*   Updated: 2023/04/11 22:37:02 by edu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account( int initial_deposit ) {
+	(void)initial_deposit;
+}
+
+Account::~Account() {
+
+}
+
+void	Account::displayAccountsInfos() {
+
+}
+
+void	Account::displayStatus() const {
+
+}
+
+void	Account::makeDeposit(int deposit) {
+	(void)deposit;
+}
+
+bool	Account::makeWithdrawal( int withdrawal ) {
+	(void)withdrawal;
+	return (false);
+}
