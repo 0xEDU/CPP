57ad7b3fdba296dff13b68b3dc37f2c6c5a6d3a9
etachott <bfwaend@gmail.com>
2023-04-22 04:44:43 -0300

    feat: added ex04/m01 functions to main and Makefile

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -6,11 +6,20 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 16:05:31 by etachott          #+#    #+#             */
-/*   Updated: 2023/04/21 16:06:44 by etachott         ###   ########.fr       */
+/*   Updated: 2023/04/22 03:17:54 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main (int argc, char *argv[])
-{
-	return 0;
+#include "File.hpp"
+#include <string>
+
+int main (int argc, char *argv[]) {
+	if (!validate_file(argc, argv))
+		return (-1);
+	File file(argv[1]);
+	std::string line;
+	while (std::getline(file.stream, line)) {
+		std::cout << line << std::endl;
+	}
+	return (0);
 }
