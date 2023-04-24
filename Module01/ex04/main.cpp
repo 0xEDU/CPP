1596c12b03360cb096285f7e28e5e3638d23a272
0xEDU <bfwaend@gmail.com>
2023-04-23 21:03:23 -0300

    feat: started to added ex04/m01 replacement feature

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 16:05:31 by etachott          #+#    #+#             */
-/*   Updated: 2023/04/22 03:17:54 by etachott         ###   ########.fr       */
+/*   Updated: 2023/04/23 20:58:12 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,9 @@ int main (int argc, char *argv[]) {
 		return (-1);
 	File file(argv[1]);
 	std::string line;
+	std::string s1(argv[2]);
 	while (std::getline(file.stream, line)) {
-		std::cout << line << std::endl;
+		std::cout << line.find(s1) << std::endl;
 	}
 	return (0);
 }
