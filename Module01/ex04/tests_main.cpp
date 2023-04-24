cd2cedb41bca57d562192680558277eab589f17b
0xEDU <bfwaend@gmail.com>
2023-04-23 21:01:57 -0300

    tests: added ex04/m01 tests for File class

--- Module01/ex04/tests_main.cpp
+++ Module01/ex04/tests_main.cpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 16:07:24 by etachott          #+#    #+#             */
-/*   Updated: 2023/04/22 02:59:21 by etachott         ###   ########.fr       */
+/*   Updated: 2023/04/23 20:39:47 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,24 +21,37 @@ void	print_result(bool result, bool expected, const char *test) {
 		std::cout << "[" << RED << "KO" << END << "]"
 		<< " " << test << std::endl;
 	}
+	return ;
 }
 
 void	test_validation(void) {
-	bool result;
-	const char *mult_invalid[] = {"abc", "abc", "abc", NULL};
-	const char *file_invalid[] = {"sed", "random", "abc", "abc", NULL};
-	const char *valid[] = {"sed", "infile", "abc", "xyz", NULL};
+	const char	*mult_invalid[] = {"abc", "abc", "abc", NULL};
+	const char	*file_invalid[] = {"sed", "random", "abc", "abc", NULL};
+	const char	*valid[] = {"sed", "infile", "abc", "xyz", NULL};
+	bool		result;
 
-	std::cout << YELLOW << "==== INPUT VALIDATION ====" << END << std::endl;
+	std::cout << YELLOW << "==== INPUT VALIDATION TESTS ====" << END << std::endl;
 	result = validate_file(3, (char **)mult_invalid);
 	print_result(result, false, "Multiple invalid arguments");
 	result = validate_file(4, (char **)file_invalid);
 	print_result(result, false, "Invalid file");
 	result = validate_file(4, (char **)valid);
 	print_result(result, true, "Valid file");
+	return ;
+}
+
+void	test_file_class(void) {
+	File	file("infile");
+	bool	result;
+
+	std::cout << YELLOW << "==== FILE CLASS TESTS ====" << END << std::endl;
+	result = file.stream.is_open();
+	print_result(result, true, "File is open");
+	return ;
 }
 
 int main (void) {
 	test_validation();
+	test_file_class();
 	return (0);
 }
