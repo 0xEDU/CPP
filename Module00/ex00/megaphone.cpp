f9f5aefbbdba6d0df83ecde6fc7cd4638c5a39f0
etachott <bfwaend@gmail.com>
2023-05-03 12:11:43 -0300

    style: brackets

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -6,14 +6,13 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 18:38:00 by edu               #+#    #+#             */
-/*   Updated: 2023/03/26 11:50:10 by edu              ###   ########.fr       */
+/*   Updated: 2023/05/03 11:00:01 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-void	to_upper(std::string & str)
-{
+void	to_upper(std::string &str) {
 	int	index;
 
 	for (index = 0; str[index]; index++)
@@ -23,18 +22,15 @@ void	to_upper(std::string & str)
 	}
 }
 
-int	main(int argc, char *argv[])
-{
+int	main(int argc, char *argv[]) {
 	std::string	str;
 	int			index;
 
-	if (argc == 1)
-	{
+	if (argc == 1) {
 		std::cout << "* LOUD AND UNBERABLE FEEDBACK NOISE *" << std::endl;
 		return (1);
 	}
-	for (index = 1; index < argc; index++)
-	{
+	for (index = 1; index < argc; index++) {
 		str = argv[index];
 		to_upper(str);
 		std::cout << str;
