f3ded844e48bf362bf808e6fa15c1779012e9a79
etachott <bfwaend@gmail.com>
2023-05-03 10:54:59 -0300

    feat: finished ex06/m01

--- Module01/ex06/Harl.cpp
+++ Module01/ex06/Harl.cpp
@@ -1,4 +1,15 @@
-/* Amazing header */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 10:34:07 by etachott          #+#    #+#             */
+/*   Updated: 2023/05/03 10:52:33 by etachott         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Harl.hpp"
 
 Harl::Harl(void) {
@@ -10,27 +21,60 @@ Harl::~Harl(void) {
 }
 
 void	Harl::complain(std::string filter) {
-	void	(Harl::*levels[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	void		(Harl::*levels[4])(void) = {&Harl::debug, &Harl::info,
+				&Harl::warning, &Harl::error};
 	std::string	names[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int			i = 0;
+	int			flag = 0;
 
+	for (; i < 4; i++) {
+		if (filter == names[i]) {
+			flag = 1;
+			switch (i) {
+				case 0:
+					(this->*levels[i++])();
+					std::cout << std::endl;
+					// fallthrough
+				case 1:
+					(this->*levels[i++])();
+					std::cout << std::endl;
+					// fallthrough
+				case 2:
+					(this->*levels[i++])();
+					std::cout << std::endl;
+					// fallthrough
+				case 3:
+					(this->*levels[i++])();
+					break ;
+			}
+		}
+	}
+	if (!flag)
+		std::cout << "[ Probably complaining about insignificant problems ]"
+		<< std::endl;
 	return ;
 }
 
 void	Harl::debug( void ) {
 	std::cout << "[ DEBUG ]\n";
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-tripe-pickle-special-ketchup burger. I really do!\n";
+	std::cout << "I love having extra bacon for my"
+	" 7XL-double-cheese-tripe-pickle-special-ketchup burger. I really do!\n";
 	return ;
 }
 
 void	Harl::info( void ) {
 	std::cout << "[ INFO ]\n";
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+	std::cout << "I cannot believe adding extra bacon costs more money."
+	"You didn't put enough bacon in my burger! If you did, I wouldn't"
+	"be asking for more!\n";
 	return ;
 }
 
 void	Harl::warning( void ) {
 	std::cout << "[ WARNING ]\n";
-	std::cout << "I think I deserve to have some exta bacon for free. I've been coming for years whereas you started working here since last month.\n";
+	std::cout << "I think I deserve to have some exta bacon for free."
+	" I've been coming for years whereas you started working here since"
+	" last month.\n";
 	return ;
 }
 
--- Module01/ex06/main.cpp
+++ Module01/ex06/main.cpp
@@ -3,6 +3,8 @@
 #include "Harl.hpp"
 
 int	main(int argc, char *argv[]) {
+	if (argc != 2)
+		return (-1);
 	Harl harl;
 
 	harl.complain(std::string(argv[1]));
