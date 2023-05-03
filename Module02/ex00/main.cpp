8ff74ce3b52b275219bf6f0c1cfd7dbeafd2b137
etachott <bfwaend@gmail.com>
2023-05-03 16:17:02 -0300

    fix: renamed folder

---
+++ Module02/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 16:15:41 by etachott          #+#    #+#             */
+/*   Updated: 2023/05/03 16:16:43 by etachott         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int	main(void) {
+	Fixed	a;
+	Fixed	b( a );
+	Fixed	c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return (0);
+}
