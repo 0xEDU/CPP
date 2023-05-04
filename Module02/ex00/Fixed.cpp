685061206e6cbfe385552d9836935c5345e784a2
etachott <bfwaend@gmail.com>
2023-05-04 12:48:18 -0300

    feat: added full implementation of Fixed class member functions on ex00/m02

--- Module02/ex00/Fixed.cpp
+++ Module02/ex00/Fixed.cpp
@@ -6,25 +6,40 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 15:39:22 by etachott          #+#    #+#             */
-/*   Updated: 2023/05/03 15:42:38 by etachott         ###   ########.fr       */
+/*   Updated: 2023/05/04 12:25:49 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-Fixed::Fixed(void) {
+Fixed::Fixed(void) : value(0){
+	std::cout << "Default constructor called\n";
+	return ;
+}
+
+Fixed::Fixed(const Fixed &rhs) {
+	std::cout << "Copy constructor called\n";
+	*this = rhs;
 	return ;
 }
 
 Fixed::~Fixed(void) {
+	std::cout << "Destructor called\n";
 	return ;
 }
 
+Fixed	&Fixed::operator=(const Fixed &rhs) {
+	std::cout << "Copy assignment called\n";
+	this->value = rhs.getRawBits();
+	return (*this);
+}
+
 int	Fixed::getRawBits( void ) const { 
-	return (1);
+	std::cout << "getRawBits member function called\n";
+	return (this->value);
 }
 
 void	Fixed::setRawBits( int const raw ) {
-	(void) raw;
+	this->value = raw;
 	return ;
 }
