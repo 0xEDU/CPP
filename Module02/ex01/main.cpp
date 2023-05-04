afed5131aca981af656905ce4f7d427cadf92d2a
etachott <bfwaend@gmail.com>
2023-05-04 13:04:56 -0300

    feat: updated ex01/m02 main

--- Module02/ex01/main.cpp
+++ Module02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 16:15:41 by etachott          #+#    #+#             */
-/*   Updated: 2023/05/03 16:16:43 by etachott         ###   ########.fr       */
+/*   Updated: 2023/05/04 13:03:38 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,22 @@
 #include <iostream>
 
 int	main(void) {
-	Fixed	a;
-	Fixed	b( a );
-	Fixed	c;
+	Fixed		a;
+	Fixed const	b( a );
+	Fixed const	c( 42.42f );
+	Fixed const	d( b );
 
-	c = b;
+	a = Fixed( 1234.4321f );
 
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 
 	return (0);
 }
