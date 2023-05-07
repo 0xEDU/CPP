853892a60856c4561e114bd7f000fa14d44474ff
Eduardo Tachotte <58338880+0xEDU@users.noreply.github.com>
2023-05-07 20:40:51 +0000

    feat: added tests to overloads and static functions on ex02/m02

--- Module02/ex02/main.cpp
+++ Module02/ex02/main.cpp
@@ -14,18 +14,73 @@
 #include <iostream>
 
 int	main(void) {
-	Fixed		a;
-	Fixed const	b( Fixed( 5.05f ) * Fixed( 2 ) );
+	{
+		std::cout << "=== Increment and decrement operator tests ===\n";
+		Fixed		a( 5 );
 
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
+		std::cout << "a = " << a << std::endl;
+		std::cout << "raw bits = " << a.getRawBits() << std::endl;
+		std::cout << ++a << std::endl;
+		std::cout << "raw bits = " << a.getRawBits() << std::endl;
+		std::cout << a++ << std::endl;
+		std::cout << "raw bits = " << a.getRawBits() << std::endl;
+		std::cout << --a << std::endl;
+		std::cout << "raw bits = " << a.getRawBits() << std::endl;
+		std::cout << a-- << std::endl;
+		std::cout << "raw bits = " << a.getRawBits() << std::endl;
+	}
+	{
+		std::cout << "\n=== Comparison operator tests ===\n";
+		Fixed		a( 5 );
+		Fixed		b( 10.15f );
 
-	std::cout << b << std::endl;
+		std::cout << "a = " << a << " " << "b = " << b << std::endl;
+		std::cout << "a < b = " << (a < b) << std::endl;
+		std::cout << "a <= b = " << (a <= b) << std::endl;
+		std::cout << "a > b = " << (a > b) << std::endl;
+		std::cout << "a >= b = " << (a >= b) << std::endl;
+		std::cout << "a == b = " << (a == b) << std::endl;
+		std::cout << "a != b = " << (a != b) << std::endl;
+	}
+	{
+		std::cout << "\n=== Arithmetic operator tests ===\n";
+		Fixed		a( 5 );
+		Fixed		b( 10.15f );
 
-	std::cout << Fixed::max( a, b ) << std::endl;
+		std::cout << "a = " << a << " " << "b = " << b << std::endl;
+		std::cout << "a + b = " << a + b << std::endl;
+		std::cout << "a - b = " << a - b << std::endl;
+		std::cout << "a * b = " << a * b << std::endl;
+		std::cout << "a / b = " << a / b << std::endl;
+	}
+	{
+		std::cout << "\n=== Min and Max functions tets ===\n";
+		Fixed		a( 5 );
+		Fixed		b( 10.15f );
+		const Fixed		c( 8 );
+		const Fixed		d( 18.21f );
 
+		std::cout << "a = " << a << " " << "b = " << b << std::endl;
+		std::cout << "const " << c << " " << d << std::endl;
+		std::cout << "min(a, b) " << Fixed::min(a, b) << std::endl;
+		std::cout << "(const) min(a, b) " << Fixed::min(c, d) << std::endl;
+		std::cout << "max(a, b) " << Fixed::max(a, b) << std::endl;
+		std::cout << "(const) max(a, b) " << Fixed::max(c, d) << std::endl;
+	}
+	{
+		std::cout << "\n=== Subject tests ===\n";
+		Fixed		a;
+		Fixed const	b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+		std::cout << a << std::endl;
+		std::cout << ++a << std::endl;
+		std::cout << a << std::endl;
+		std::cout << a++ << std::endl;
+		std::cout << a << std::endl;
+
+		std::cout << b << std::endl;
+
+		std::cout << Fixed::max( a, b ) << std::endl;
+	}
 	return (0);
 }
