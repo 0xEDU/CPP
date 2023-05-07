c70e9db7835c3aa10a71277b4bb845362692c9a8
Eduardo Tachotte <58338880+0xEDU@users.noreply.github.com>
2023-05-07 20:58:54 +0000

    wip: adding ex03/m02 Point class implementations

---
+++ Module02/ex03/Point.hpp
@@ -0,0 +1,20 @@
+/* Amazing header */
+
+#ifndef POINT_HPP
+#define POINT_HPP
+
+#include "Fixed.h"
+
+class Point {
+    Fixed   _x;
+    Fixed   _y;
+
+    public:
+        Point();
+        Point(float x, float y);
+        Point(const Point &rhs);
+        Point &operator=(const Point &rhs);
+        ~Point();
+}
+
+#endif
