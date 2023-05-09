3cf3fc0771281d08d511668d956fce9582fcd399
etachott <bfwaend@gmail.com>
2023-05-09 16:03:20 -0300

    style: added colors to tests

--- Module02/ex03/bsp.cpp
+++ Module02/ex03/bsp.cpp
@@ -1,4 +1,14 @@
-/* Amazing header */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bsp.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 13:58:03 by etachott          #+#    #+#             */
+/*   Updated: 2023/05/08 17:44:59 by etachott         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "Point.hpp"
 
@@ -7,5 +17,5 @@ bool    bsp( Point const a, Point const b, Point const c, Point const point ) {
     (void)b;
     (void)c;
     (void)point;
-    return true;
+    return false;
 }
