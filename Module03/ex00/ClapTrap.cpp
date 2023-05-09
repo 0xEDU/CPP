1de756092917edf80c9f564b4b7fcd74d2da117b
etachott <bfwaend@gmail.com>
2023-05-09 18:49:53 -0300

    feat: added ClapTrap functions implementation in ex00/m03

--- Module03/ex00/ClapTrap.cpp
+++ Module03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 18:40:41 by etachott          #+#    #+#             */
-/*   Updated: 2023/05/09 18:47:21 by etachott         ###   ########.fr       */
+/*   Updated: 2023/05/09 18:49:17 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,18 @@ ClapTrap	&ClapTrap::operator=(const ClapTrap &rhs) {
 	this->_energyPoints = rhs._energyPoints;
 	this->_attackDamage = rhs._attackDamage;
 	return (*this);
+}
+
+void	ClapTrap::attack(const std::string &target) {
+	return ;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount) {
+	return ;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount) {
+	return ;
 }
 
 ClapTrap::~ClapTrap() {
