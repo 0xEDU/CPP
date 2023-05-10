3e3da2a84668a7954a05258734b2267513873501
etachott <bfwaend@gmail.com>
2023-05-10 19:09:03 -0300

    feat: added new constructor for FragTrap on ex02/m03

--- Module03/ex02/FragTrap.hpp
+++ Module03/ex02/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 18:31:08 by etachott          #+#    #+#             */
-/*   Updated: 2023/05/10 18:33:48 by etachott         ###   ########.fr       */
+/*   Updated: 2023/05/10 18:55:07 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,12 @@ class FragTrap : public ClapTrap {
 
 	public:
 		FragTrap ();
+		FragTrap (std::string name);
 		FragTrap (const FragTrap  &);
 		FragTrap  &operator=(const FragTrap  &);
 		~FragTrap ();
+
+		void	highFiveGuys(void);
 };
 
 #endif // !FRAGTRAP_HPP
