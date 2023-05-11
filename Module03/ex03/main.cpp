408257e80526a2d2a667cc0f3190a67d0e6003a6
etachott <bfwaend@gmail.com>
2023-05-10 21:18:16 -0300

    tests: added tests for DiamondTrap class

--- Module03/ex03/main.cpp
+++ Module03/ex03/main.cpp
@@ -6,54 +6,54 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 15:11:44 by etachott          #+#    #+#             */
-/*   Updated: 2023/05/10 19:06:28 by etachott         ###   ########.fr       */
+/*   Updated: 2023/05/10 20:59:08 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "FragTrap.hpp"
+#include "DiamondTrap.hpp"
 
 int	main(void) {
-	std::cout << "== FragTrap operates normally ==" << std::endl;
+	std::cout << "== DiamondTrap operates normally ==" << std::endl;
 	{
-		FragTrap a("Hal");
+		DiamondTrap a("Hal");
 		ClapTrap b("Joy");
 
-		a.attack("Atrocitus");
+		// a.attack("Atrocitus");
 		a.takeDamage(3);
 		a.beRepaired(5);
 		b.attack("Atrocitus");
 		b.takeDamage(3);
 		b.beRepaired(5);
 	}
-	std::cout << "\n== FragTrap dies ==" << std::endl;
+	std::cout << "\n== DiamondTrap dies ==" << std::endl;
 	{
-		FragTrap a("Guy");
+		DiamondTrap a("Guy");
 
-		a.attack("Parallax");
+		// a.attack("Parallax");
 		a.takeDamage(110);
 		a.beRepaired(5);
 	}
-	std::cout << "\n== FragTrap attacks and is out of energy ==" << std::endl;
+	std::cout << "\n== DiamondTrap attacks and is out of energy ==" << std::endl;
 	{
-		FragTrap a("John");
+		DiamondTrap a("John");
 
 		for (int i = 0; i < 101; i++) {
-			a.attack("Sinestro");
+			// a.attack("Sinestro");
 		}
 	}
-	std::cout << "\n== FragTrap is repaired and runs out of energy =="
+	std::cout << "\n== DiamondTrap is repaired and runs out of energy =="
 		<< std::endl;
 	{
-		FragTrap a("Kyle");
+		DiamondTrap a("Kyle");
 
 		for (int i = 0; i < 101; i++) {
 			a.beRepaired(5);
 		}
 	}
-	std::cout << "\n== FragTrap high five =="
+	std::cout << "\n== DiamondTrap high five =="
 		<< std::endl;
 	{
-		FragTrap a("Simon");
+		DiamondTrap a("Simon");
 
 		a.highFiveGuys();
 	}
