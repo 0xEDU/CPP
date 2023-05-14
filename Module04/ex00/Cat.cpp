cf032be2b114a4ede27736fbc2aa40dd304964e7
0xEDU <bfwaend@gmail.com>
2023-05-14 16:49:36 -0300

    feat: add message for constructor call for Cat class on ex00/m04

--- Module04/ex00/Cat.cpp
+++ Module04/ex00/Cat.cpp
@@ -6,13 +6,14 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 19:22:24 by edu               #+#    #+#             */
-/*   Updated: 2023/05/14 11:51:12 by edu              ###   ########.fr       */
+/*   Updated: 2023/05/14 16:43:28 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 
 Cat::Cat() {
+	std::cout << "Cat created" << std::endl;
 	this->type = "Cat";
 	return ;
 }
@@ -28,10 +29,11 @@ Cat	&Cat::operator=(const Cat &rhs) {
 }
 
 Cat::~Cat() {
+	std::cout << "Cat destroyed" << std::endl;
 	return ;
 }
 
-void	Cat::makeSound() {
+void	Cat::makeSound() const {
 	std::cout << "MEOW MEOW" << std::endl;
 	return ;
 }
