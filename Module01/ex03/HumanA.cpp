a2c4417394472b1f35d09b603895973ef45d3cba
etachott <bfwaend@gmail.com>
2023-05-20 14:44:06 -0300

    refactor: moved initialization to initialization list on class HumanB on ex03/m01

--- Module01/ex03/HumanA.cpp
+++ Module01/ex03/HumanA.cpp
@@ -6,15 +6,13 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 15:15:19 by etachott          #+#    #+#             */
-/*   Updated: 2023/04/20 15:38:20 by etachott         ###   ########.fr       */
+/*   Updated: 2023/05/20 14:38:06 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 
-HumanA::HumanA(std::string name, Weapon &weapon) : weapon(weapon)
-{
-	this->name = name;
+HumanA::HumanA(std::string name, Weapon &weapon) : name(name), weapon(weapon) {
 	return ;
 }
 
