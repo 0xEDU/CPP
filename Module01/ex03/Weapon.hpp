e2f1877a3ba2dc55d8c78735bf168420222c9060
etachott <bfwaend@gmail.com>
2023-05-20 14:45:49 -0300

    style: adequated Weapon class to coding style on ex03/m01

--- Module01/ex03/Weapon.hpp
+++ Module01/ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 15:36:05 by etachott          #+#    #+#             */
-/*   Updated: 2023/04/20 16:17:52 by etachott         ###   ########.fr       */
+/*   Updated: 2023/05/19 15:05:25 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ class Weapon {
 		Weapon(const std::string);
 		~Weapon();
 
-		const std::string	&getType(void);
-		void				setType(std::string type);
+		const std::string &getType(void);
+		void setType(std::string type);
 
 	private:
-		std::string	_type;
+		std::string _type;
 };
 
 #endif // !WEAPON
