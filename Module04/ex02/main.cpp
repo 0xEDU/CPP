c76a51abe95f81bf401993d3fa75dd28cab37b22
etachott <bfwaend@gmail.com>
2023-05-23 17:42:50 -0300

    chore: update main to use AAnimal class on ex02/m04

--- Module04/ex02/main.cpp
+++ Module04/ex02/main.cpp
@@ -6,15 +6,19 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 18:25:00 by edu               #+#    #+#             */
-/*   Updated: 2023/05/23 16:04:02 by etachott         ###   ########.fr       */
+/*   Updated: 2023/05/23 17:39:04 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Animal.hpp"
+#include "AAnimal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
 
 int main (void) {
+	// std::cout << YELLOW << "=== ABSTRACT CLASS TESTS ===" << END << std::endl;
+	// {
+	// 	AAnimal a;
+	// }
 	std::cout << YELLOW << "=== DEEP COPY TESTS ===" << END << std::endl;
 	std::cout << "CAT DEEP COPY" << std::endl;
 	{
@@ -36,15 +40,15 @@ int main (void) {
 	}
 	std::cout << YELLOW << "\n=== SUBJECT TESTS ===" << END << std::endl;
 	{
-		const Animal *dog = new Dog();
-		const Animal *cat = new Cat();
+		const AAnimal *dog = new Dog();
+		const AAnimal *cat = new Cat();
 
 		delete dog;
 		delete cat;
 	}
 	std::cout << YELLOW << "\n=== ARRAY TESTS ===" << END << std::endl;
 	{
-		const Animal *animals[10];
+		const AAnimal *animals[10];
 		std::cout << "CREATING ARRAY" << std::endl;
 		for (int i = 0; i < 10; i++) {
 			if (i < 5)
