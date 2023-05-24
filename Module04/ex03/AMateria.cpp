405ead1e995b2eaaf290ef586a2a2ec1fcaae4b0
0xEDU <bfwaend@gmail.com>
2023-05-23 23:38:31 -0300

    feat: add missing AMateria class implementation on ex03/m04

---
+++ Module04/ex03/AMateria.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 23:34:26 by edu               #+#    #+#             */
+/*   Updated: 2023/05/23 23:36:54 by edu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AMateria.hpp"
+
+AMateria::AMateria(std::string const &type) : type(type) {
+	return ;
+}
+
+std::string const &AMateria::getType() const {
+	return this->type;
+}
+
+void AMateria::use(ICharacter &target) {
+	(void)target;
+	return ;
+}
