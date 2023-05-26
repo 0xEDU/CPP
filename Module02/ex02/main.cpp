931aa34b3d8744dff86487702f6149859292da55
etachott <bfwaend@gmail.com>
2023-05-26 17:47:41 -0300

    tests: add colors and more verbosity on ex02/m02

--- Module02/ex02/main.cpp
+++ Module02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 16:15:41 by etachott          #+#    #+#             */
-/*   Updated: 2023/05/05 15:09:25 by etachott         ###   ########.fr       */
+/*   Updated: 2023/05/26 17:45:57 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,28 @@
 
 int	main(void) {
 	{
-		std::cout << "=== Increment and decrement operator tests ===\n";
+		std::cout << YELLOW
+			<< "=== Increment and decrement operator tests ===\n" << END;
 		Fixed		a( 5 );
 
 		std::cout << "a = " << a << std::endl;
 		std::cout << "raw bits = " << a.getRawBits() << std::endl;
-		std::cout << ++a << std::endl;
+		std::cout << "++a = " << ++a << std::endl;
 		std::cout << "raw bits = " << a.getRawBits() << std::endl;
-		std::cout << a++ << std::endl;
+		std::cout << "a++ = " << a++ << std::endl;
 		std::cout << "raw bits = " << a.getRawBits() << std::endl;
-		std::cout << --a << std::endl;
+		std::cout << "--a = " << --a << std::endl;
 		std::cout << "raw bits = " << a.getRawBits() << std::endl;
-		std::cout << a-- << std::endl;
+		std::cout << "a-- = " << a-- << std::endl;
 		std::cout << "raw bits = " << a.getRawBits() << std::endl;
 	}
 	{
-		std::cout << "\n=== Comparison operator tests ===\n";
+		std::cout << YELLOW << "\n=== Comparison operator tests ===\n" << END;
 		Fixed		a( 5 );
 		Fixed		b( 10.15f );
 
-		std::cout << "a = " << a << " " << "b = " << b << std::endl;
+		std::cout << "a = " << a << " " << "| b = " << b << std::endl;
+		std::cout << std::endl;
 		std::cout << "a < b = " << (a < b) << std::endl;
 		std::cout << "a <= b = " << (a <= b) << std::endl;
 		std::cout << "a > b = " << (a > b) << std::endl;
@@ -42,33 +44,34 @@ int	main(void) {
 		std::cout << "a == b = " << (a == b) << std::endl;
 		std::cout << "a != b = " << (a != b) << std::endl;
 	}
-	{
-		std::cout << "\n=== Arithmetic operator tests ===\n";
+	{ std::cout << YELLOW << "\n=== Arithmetic operator tests ===\n" << END;
 		Fixed		a( 5 );
 		Fixed		b( 10.15f );
 
-		std::cout << "a = " << a << " " << "b = " << b << std::endl;
+		std::cout << "a = " << a << " " << "| b = " << b << std::endl;
+		std::cout << std::endl;
 		std::cout << "a + b = " << a + b << std::endl;
 		std::cout << "a - b = " << a - b << std::endl;
 		std::cout << "a * b = " << a * b << std::endl;
 		std::cout << "a / b = " << a / b << std::endl;
 	}
 	{
-		std::cout << "\n=== Min and Max functions tets ===\n";
+		std::cout << YELLOW << "\n=== Min and Max functions tets ===\n" << END;
 		Fixed		a( 5 );
 		Fixed		b( 10.15f );
 		const Fixed		c( 8 );
 		const Fixed		d( 18.21f );
 
-		std::cout << "a = " << a << " " << "b = " << b << std::endl;
-		std::cout << "const " << c << " " << d << std::endl;
-		std::cout << "min(a, b) " << Fixed::min(a, b) << std::endl;
-		std::cout << "(const) min(a, b) " << Fixed::min(c, d) << std::endl;
-		std::cout << "max(a, b) " << Fixed::max(a, b) << std::endl;
-		std::cout << "(const) max(a, b) " << Fixed::max(c, d) << std::endl;
+		std::cout << "a = " << a << " " << "| b = " << b << std::endl;
+		std::cout << "const c = " << c << " d = " << d << std::endl;
+		std::cout << std::endl;
+		std::cout << "min(a, b) = " << Fixed::min(a, b) << std::endl;
+		std::cout << "(const) min(a, b) = " << Fixed::min(c, d) << std::endl;
+		std::cout << "max(a, b) = " << Fixed::max(a, b) << std::endl;
+		std::cout << "(const) max(a, b) = " << Fixed::max(c, d) << std::endl;
 	}
 	{
-		std::cout << "\n=== Subject tests ===\n";
+		std::cout << YELLOW << "\n=== Subject tests ===\n" << END;
 		Fixed		a;
 		Fixed const	b( Fixed( 5.05f ) * Fixed( 2 ) );
 
