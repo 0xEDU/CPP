e6fb93868ae6c081756e168477c9e4e916f4894b
etachott <bfwaend@gmail.com>
2023-05-27 14:25:49 -0300

    tests: finish writing tests for ex03/m02

--- Module02/ex03/main.cpp
+++ Module02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 16:15:41 by etachott          #+#    #+#             */
-/*   Updated: 2023/05/08 17:44:46 by etachott         ###   ########.fr       */
+/*   Updated: 2023/05/27 14:23:22 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,39 @@ static void	assert(bool result, bool expected, const char *test) {
 }
 
 int	main(void) {
-	Point	a(0, 0);
-	Point	b(2, 0);
-	Point	c(2, 2);
-	Point	p(1.5f, 1);
 
-	assert(bsp(a, b, c, p), true, "Point is inside the triangle");
+	std::cout << YELLOW << "=== TESTS ===" << END << std::endl;
+	{
+		Point	a(0, 0);
+		Point	b(2, 0);
+		Point	c(2, 2);
+		Point	p(3, 3);
+
+		assert(bsp(a, b, c, p), false, "Point is outside the triangle");
+	}
+	{
+		Point	a(0, 0);
+		Point	b(2, 0);
+		Point	c(2, 2);
+		Point	p(1.5f, 1);
+
+		assert(bsp(a, b, c, p), true, "Point is inside the triangle");
+	}
+	{
+		Point	a(0, 0);
+		Point	b(2, 0);
+		Point	c(2, 2);
+		Point	p(2, 2);
+
+		assert(bsp(a, b, c, p), false, "Point is a vertex of the triangle");
+	}
+	{
+		Point	a(0, 0);
+		Point	b(2, 0);
+		Point	c(2, 2);
+		Point	p(1, 2);
+
+		assert(bsp(a, b, c, p), false, "Point is on edge of the triangle");
+	}
 	return (0);
 }
