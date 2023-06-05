ea4223249a751542a7f780984e215e08d8f46282
etachott <bfwaend@gmail.com>
2023-06-05 19:54:31 -0300

    style: add '=' to some test messages on ex02/m04

--- Module04/ex02/main.cpp
+++ Module04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 18:25:00 by edu               #+#    #+#             */
-/*   Updated: 2023/05/23 17:39:04 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/05 19:53:57 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int main (void) {
 	// 	AAnimal a;
 	// }
 	std::cout << YELLOW << "=== DEEP COPY TESTS ===" << END << std::endl;
-	std::cout << "CAT DEEP COPY" << std::endl;
+	std::cout << "= CAT DEEP COPY =" << std::endl;
 	{
 		Cat cat;
 		Cat catto = cat;
@@ -49,14 +49,14 @@ int main (void) {
 	std::cout << YELLOW << "\n=== ARRAY TESTS ===" << END << std::endl;
 	{
 		const AAnimal *animals[10];
-		std::cout << "CREATING ARRAY" << std::endl;
+		std::cout << "= CREATING ARRAY =" << std::endl;
 		for (int i = 0; i < 10; i++) {
 			if (i < 5)
 				animals[i] = new Cat();
 			else
 				animals[i] = new Dog();
 		}
-		std::cout << "\nDELETING ARRAY" << std::endl;
+		std::cout << "\n= DELETING ARRAY =" << std::endl;
 		for (int i = 0; i < 10; i++) {
 			delete animals[i];
 		}
