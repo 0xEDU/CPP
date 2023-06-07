a7a001e4aad051372b2347235566e75365e55317
etachott <bfwaend@gmail.com>
2023-06-07 20:53:15 -0300

    tests: add tests for Form class on ex01/m05

--- Module05/ex01/main.cpp
+++ Module05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 08:19:21 by edu               #+#    #+#             */
-/*   Updated: 2023/06/06 20:02:05 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/07 20:46:04 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,93 +15,24 @@
 
 int main(void) {
 	std::cout << YELLOW << "=== CREATING A FORM ===" << END << std::endl;
-	std::cout << YELLOW << "\n=== BUREAUCRAT SIGNING A FORM ===" << END << std::endl;
 	{
-		try {
-			Bureaucrat a("Hermes", 50);
-			Form b("Visa", 100, 30);
+		Form a("Affidavit", 100, 30);
 
-			std::cout << a << std::endl;
-			std::cout << "NAME = " << a.getName() << std::endl;
-			std::cout << "GRADE = " << a.getGrade() << std::endl;
-		} catch (const Bureaucrat::GradeTooHighException &e) {
-			std::cerr << e.what();
-		} catch (const Bureaucrat::GradeTooLowException &e) {
-			std::cerr << e.what();
-		}
+		std::cout << a << std::endl;
 	}
-	std::cout << YELLOW << "\n=== LOW GRADE BUREAUCRAT ===" << END << std::endl;
+	std::cout << YELLOW << "=== BUREAUCRAT SIGNING A FORM ===" << END << std::endl;
 	{
-		try {
-			Bureaucrat a("Hermes", 200);
+		Bureaucrat a("Hermes", 50);
+		Form b("Visa", 100, 30);
 
-			std::cout << a << std::endl;
-		} catch (const Bureaucrat::GradeTooHighException &e) {
-			std::cerr << e.what() << std::endl;
-		} catch (const Bureaucrat::GradeTooLowException &e) {
-			std::cerr << e.what() << std::endl;
-		}
+		a.signForm(b);
 	}
-	std::cout << YELLOW << "\n=== HIGH GRADE BUREAUCRAT ===" << END << std::endl;
+	std::cout << YELLOW << "\n=== LOW GRADE BUREAUCRAT TRIES TO SIGN A FORM ===" << END << std::endl;
 	{
-		try {
-			Bureaucrat a("Hermes", 0);
+		Bureaucrat a("Hermes", 150);
+		Form b("Visa", 100, 30);
 
-			std::cout << a << std::endl;
-		} catch (const Bureaucrat::GradeTooHighException &e) {
-			std::cerr << e.what() << std::endl;
-		} catch (const Bureaucrat::GradeTooLowException &e) {
-			std::cerr << e.what() << std::endl;
-		}
-	}
-	std::cout << YELLOW << "\n=== INCREMENT AND DECREMENT ===" << END << std::endl;
-	{
-		try {
-			Bureaucrat a("Hermes", 100);
-
-			std::cout << a << std::endl;
-			std::cout << "ORIGINAL GRADE = " << a.getGrade() << std::endl;
-			a.incrementGrade();
-			std::cout << "INCREMENTED = " << a.getGrade() << std::endl;
-			a.decrementGrade();
-			std::cout << "DECREMENTED = " << a.getGrade() << std::endl;
-		} catch (const Bureaucrat::GradeTooHighException &e) {
-			std::cerr << e.what() << std::endl;
-		} catch (const Bureaucrat::GradeTooLowException &e) {
-			std::cerr << e.what() << std::endl;
-		}
-	}
-	std::cout << YELLOW << "\n=== OUT OF BOUNDS INCREMENT ==="
-		<< END << std::endl;
-	{
-		try {
-			Bureaucrat a("Hermes", 1);
-
-			std::cout << a << std::endl;
-			std::cout << "ORIGINAL GRADE = " << a.getGrade() << std::endl;
-			std::cout << "CAUSING EXCEPTION BY INCREMENT..." << std::endl;
-			a.incrementGrade();
-		} catch (const Bureaucrat::GradeTooHighException &e) {
-			std::cerr << e.what() << std::endl;
-		} catch (const Bureaucrat::GradeTooLowException &e) {
-			std::cerr << e.what() << std::endl;
-		}
-	}
-	std::cout << YELLOW << "\n=== OUT OF BOUNDS DECREMENT ==="
-		<< END << std::endl;
-	{
-		try {
-			Bureaucrat a("Hermes", 150);
-
-			std::cout << a << std::endl;
-			std::cout << "ORIGINAL GRADE = " << a.getGrade() << std::endl;
-			std::cout << "CAUSING EXCEPTION BY DECREMENT..." << std::endl;
-			a.decrementGrade();
-		} catch (const Bureaucrat::GradeTooHighException &e) {
-			std::cerr << e.what() << std::endl;
-		} catch (const Bureaucrat::GradeTooLowException &e) {
-			std::cerr << e.what() << std::endl;
-		}
+		a.signForm(b);
 	}
 	return (0);
 }
