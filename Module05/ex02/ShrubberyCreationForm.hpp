68fa47243a9194bef4b03990aee71ca713a7988f
etachott <bfwaend@gmail.com>
2023-06-09 18:22:16 -0300

    refactor: remove unused exceptions from ShrubberyCreationForm class on ex02/m05

--- Module05/ex02/ShrubberyCreationForm.hpp
+++ Module05/ex02/ShrubberyCreationForm.hpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 22:01:29 by etachott          #+#    #+#             */
-/*   Updated: 2023/06/08 02:28:36 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/09 17:51:39 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,21 +42,6 @@ class ShrubberyCreationForm : public AForm {
 		std::string _target;
 
 	public:
-		class SignGradeTooLowException : public std::exception {
-			public:
-				const char	*what(void) const throw();
-		};
-
-		class ExecGradeTooLowException : public std::exception {
-			public:
-				const char	*what(void) const throw();
-		};
-
-		class FormIsNotSigned : public std::exception {
-			public:
-				const char	*what(void) const throw();
-		};
-
 		ShrubberyCreationForm();
 		ShrubberyCreationForm(const ShrubberyCreationForm &);
 		ShrubberyCreationForm(std::string target);
