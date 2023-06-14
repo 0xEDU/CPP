10ca24043081506e90f36e8d7c5acf4275216df5
0xEDU <bfwaend@gmail.com>
2023-06-14 16:08:39 -0300

    fix: add parent initializartion on copy constructor on concrete forms on ex03/m05

--- Module05/ex03/PresidentialPardonForm.cpp
+++ Module05/ex03/PresidentialPardonForm.cpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 20:28:22 by etachott          #+#    #+#             */
-/*   Updated: 2023/06/09 20:36:21 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/14 15:48:17 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ PresidentialPardonForm::PresidentialPardonForm(std::string target)
 	return ;
 }
 
-PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &rhs) {
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &rhs)
+	: AForm("PresidentialPardonForm", 25, 5, rhs._target) {
 	*this = rhs;
 	return ;
 }
