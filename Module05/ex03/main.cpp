fa19c69b0564a0b03761564db3e51e63cce92823
0xEDU <bfwaend@gmail.com>
2023-06-14 16:09:12 -0300

    tests: finish tests for Intern class on ex03/m05

--- Module05/ex03/main.cpp
+++ Module05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 08:19:21 by edu               #+#    #+#             */
-/*   Updated: 2023/06/13 18:45:43 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/14 16:05:53 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,60 @@
 #include "Intern.hpp"
 
 int main(void) {
+	std::cout << YELLOW << "\n=== CREATING AN INVALID FORM ==="
+		<< END << std::endl;
+	{
+		Intern someRandomIntern;
+		Bureaucrat a("Hermes", 10);
+		AForm *invf;
+
+		try {
+			invf = someRandomIntern.makeForm("invalid form", "Bender");
+			a.signForm(*invf);
+			invf->execute(a);
+		} catch(std::exception &e) {
+			std::cerr << e.what() << std::endl;
+		}
+		delete invf;
+	}
 	std::cout << YELLOW << "\n=== CREATING A ROBOTOMY REQUEST ==="
 		<< END << std::endl;
 	{
 		Intern someRandomIntern;
+		Bureaucrat a("Hermes", 10);
 		AForm *rrf;
 
 		rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+		a.signForm(*rrf);
+		rrf->execute(a);
 
 		delete rrf;
+	}
+	std::cout << YELLOW << "\n=== CREATING A PRESIDENTIAL PARDON ==="
+		<< END << std::endl;
+	{
+		Intern someRandomIntern;
+		Bureaucrat a("Hermes", 3);
+		AForm *ppf;
+
+		ppf = someRandomIntern.makeForm("presidential pardon", "Bender");
+		a.signForm(*ppf);
+		ppf->execute(a);
+
+		delete ppf;
+	}
+	std::cout << YELLOW << "\n=== CREATING A SHRUBBERY CREATION ==="
+		<< END << std::endl;
+	{
+		Intern someRandomIntern;
+		Bureaucrat a("Hermes", 3);
+		AForm *scf;
+
+		scf = someRandomIntern.makeForm("shrubbery creation", "home");
+		a.signForm(*scf);
+		scf->execute(a);
+
+		delete scf;
 	}
 	return (0);
 }
