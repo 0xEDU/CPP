6eda45030e8fbd92135afa5efa086fd6738568f8
etachott <bfwaend@gmail.com>
2023-06-17 18:08:05 -0300

    chore: add main.cpp on ex01/m06

---
+++ Module06/ex01/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/17 17:56:37 by etachott          #+#    #+#             */
+/*   Updated: 2023/06/17 18:03:21 by etachott         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdint>
+#include <iostream>
+
+int main (int argc, char *argv[])
+{
+	// Data *importantData = {};
+	// Data *importantDataCopy;
+	uintptr_t ptr;
+
+	// ptr = Serializer::serialize(importantData);
+	std::cout << ptr;
+	// importantDataCopy = Serializer::deserialize(ptr);
+	return 0;
+}
