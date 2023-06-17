38d9749f2c9961c87068a5f0cc0b7e841e9666d4
etachott <bfwaend@gmail.com>
2023-06-17 18:21:23 -0300

    feat: update main on ex01/m06

--- Module06/ex01/main.cpp
+++ Module06/ex01/main.cpp
@@ -6,21 +6,24 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 17:56:37 by etachott          #+#    #+#             */
-/*   Updated: 2023/06/17 18:03:21 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/17 18:20:11 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cstdint>
-#include <iostream>
+#include "Serializer.hpp"
 
 int main (int argc, char *argv[])
 {
-	// Data *importantData = {};
-	// Data *importantDataCopy;
+	Data importantData = {
+		.role = "user",
+		.name = "marvin",
+		.password = 123
+	};
+	Data *importantDataCopy;
 	uintptr_t ptr;
 
-	// ptr = Serializer::serialize(importantData);
+	ptr = Serializer::serialize(&importantData);
 	std::cout << ptr;
-	// importantDataCopy = Serializer::deserialize(ptr);
+	importantDataCopy = Serializer::deserialize(ptr);
 	return 0;
 }
