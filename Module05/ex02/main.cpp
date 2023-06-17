e9de525982c43126f702fa6f3c0f5aed6e3451b9
etachott <bfwaend@gmail.com>
2023-06-17 18:46:35 -0300

    tests: update tests to remove unnecessary try-catch block on ex02/m05

--- Module05/ex02/main.cpp
+++ Module05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 08:19:21 by edu               #+#    #+#             */
-/*   Updated: 2023/06/09 20:33:02 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/17 18:44:41 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,48 +23,36 @@ int main(void) {
 	std::cout << YELLOW << "\n=== LOW GRADE TO SIGN FORM ==="
 		<< END << std::endl;
 	{
-		try {
-			AForm *a = new ShrubberyCreationForm("home");
-			Bureaucrat b("Hermes", 150);
+		AForm *a = new ShrubberyCreationForm("home");
+		Bureaucrat b("Hermes", 150);
 
-			b.signForm(*a); // Grade should be equal or higher than 147!
-			a->execute(b);
+		b.signForm(*a); // Grade should be equal or higher than 147!
+		b.executeForm(*a);
 
-			delete a;
-		} catch(std::exception &e) {
-			std::cerr << e.what() << std::endl;
-		}
+		delete a;
 	}
 	std::cout << YELLOW << "\n=== LOW GRADE TO EXECUTE FORM ==="
 		<< END << std::endl;
 	{
-		try {
-			AForm *a = new ShrubberyCreationForm("home");
-			Bureaucrat b("Hermes", 140);
+		AForm *a = new ShrubberyCreationForm("home");
+		Bureaucrat b("Hermes", 140);
 
-			b.signForm(*a);
-			a->execute(b); // Grade should be equal or higher than 137!
+		b.signForm(*a);
+		b.executeForm(*a); // Grade should be equal or higher than 137!
 
-			delete a;
-		} catch(std::exception &e) {
-			std::cerr << e.what() << std::endl;
-		}
+		delete a;
 	}
 	std::cout << YELLOW << "\n=== CREATING A SHRUBBERY CREATION FORM ==="
 		<< END << std::endl;
 	{
-		try {
-			AForm *a = new ShrubberyCreationForm("home");
-			Bureaucrat b("Hermes", 130);
-			Bureaucrat c("Mercury", 1);
+		AForm *a = new ShrubberyCreationForm("home");
+		Bureaucrat b("Hermes", 130);
+		Bureaucrat c("Mercury", 1);
 
-			b.signForm(*a);
-			a->execute(c);
+		b.signForm(*a);
+		b.executeForm(*a);
 
-			delete a;
-		} catch(std::exception &e) {
-			std::cerr << e.what() << std::endl;
-		}
+		delete a;
 	}
 	/* RobotomyRequestForm tests */
 	std::cout << RED << "\n\n==== ROBOTOMY REQUEST FORM TESTS ===="
@@ -72,48 +60,36 @@ int main(void) {
 	std::cout << YELLOW << "\n== LOW GRADE TO SIGN FORM ==="
 		<< END << std::endl;
 	{
-		try {
-			AForm *a = new RobotomyRequestForm("home");
-			Bureaucrat b("Hermes", 73);
+		AForm *a = new RobotomyRequestForm("home");
+		Bureaucrat b("Hermes", 73);
 
-			b.signForm(*a); // Grade should be equal or higher than 72!
-			a->execute(b);
+		b.signForm(*a); // Grade should be equal or higher than 72!
+		b.executeForm(*a);
 
-			delete a;
-		} catch(std::exception &e) {
-			std::cerr << e.what() << std::endl;
-		}
+		delete a;
 	}
 	std::cout << YELLOW << "\n=== LOW GRADE TO EXECUTE FORM ===" 
 		<< END << std::endl;
 	{
-		try {
-			AForm *a = new RobotomyRequestForm("home");
-			Bureaucrat b("Hermes", 48);
+		AForm *a = new RobotomyRequestForm("home");
+		Bureaucrat b("Hermes", 48);
 
-			b.signForm(*a);
-			a->execute(b); // Grade should be equal or higher than 45!
+		b.signForm(*a);
+		b.executeForm(*a); // Grade should be equal or higher than 45!
 
-			delete a;
-		} catch(std::exception &e) {
-			std::cerr << e.what() << std::endl;
-		}
+		delete a;
 	}
 	std::cout << YELLOW << "\n=== CREATING A ROBOTOMY REQUEST FORM ==="
 		<< END << std::endl;
 	{
-		try {
-			AForm *a = new RobotomyRequestForm("Arthur");
-			Bureaucrat b("Hermes", 42);
-			Bureaucrat c("Mercury", 5);
+		AForm *a = new RobotomyRequestForm("Arthur");
+		Bureaucrat b("Hermes", 42);
+		Bureaucrat c("Mercury", 5);
 
-			b.signForm(*a);
-			a->execute(c);
+		b.signForm(*a);
+		c.executeForm(*a);
 
-			delete a;
-		} catch(std::exception &e) {
-			std::cerr << e.what() << std::endl;
-		}
+		delete a;
 	}
 	/* PresidentialPardonForm tests */
 	std::cout << RED << "\n\n==== PRESIDENTIAL PARDON FORM TESTS ===="
@@ -121,47 +97,35 @@ int main(void) {
 	std::cout << YELLOW << "\n== LOW GRADE TO SIGN FORM ==="
 		<< END << std::endl;
 	{
-		try {
-			AForm *a = new PresidentialPardonForm("home");
-			Bureaucrat b("Hermes", 25);
+		AForm *a = new PresidentialPardonForm("home");
+		Bureaucrat b("Hermes", 25);
 
-			b.signForm(*a); // Grade should be equal or higher than 25!
-			a->execute(b);
+		b.signForm(*a); // Grade should be equal or higher than 25!
+		b.executeForm(*a);
 
-			delete a;
-		} catch(std::exception &e) {
-			std::cerr << e.what() << std::endl;
-		}
+		delete a;
 	}
 	std::cout << YELLOW << "\n=== LOW GRADE TO EXECUTE FORM ===" 
 		<< END << std::endl;
 	{
-		try {
-			AForm *a = new PresidentialPardonForm("home");
-			Bureaucrat b("Hermes", 6);
+		AForm *a = new PresidentialPardonForm("home");
+		Bureaucrat b("Hermes", 6);
 
-			b.signForm(*a);
-			a->execute(b); // Grade should be equal or higher than 5!
+		b.signForm(*a);
+		b.executeForm(*a); // Grade should be equal or higher than 5!
 
-			delete a;
-		} catch(std::exception &e) {
-			std::cerr << e.what() << std::endl;
-		}
+		delete a;
 	}
 	std::cout << YELLOW << "\n=== CREATING A ROBOTOMY REQUEST FORM ==="
 		<< END << std::endl;
 	{
-		try {
-			AForm *a = new PresidentialPardonForm("Bender");
-			Bureaucrat b("Zaphod", 1);
+		AForm *a = new PresidentialPardonForm("Bender");
+		Bureaucrat b("Zaphod", 1);
 
-			b.signForm(*a);
-			a->execute(b);
+		b.signForm(*a);
+		b.executeForm(*a);
 
-			delete a;
-		} catch(std::exception &e) {
-			std::cerr << e.what() << std::endl;
-		}
+		delete a;
 	}
 	return (0);
 }
