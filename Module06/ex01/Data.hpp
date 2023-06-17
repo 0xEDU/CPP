65bd7c11584fa413e183bf31b1e5705dbe08f637
etachott <bfwaend@gmail.com>
2023-06-17 18:20:53 -0300

    feat: add Data structure on ex01/m06

---
+++ Module06/ex01/Data.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Data.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/17 18:13:42 by etachott          #+#    #+#             */
+/*   Updated: 2023/06/17 18:15:02 by etachott         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DATA_HPP
+#define DATA_HPP
+
+#include <iostream>
+
+struct Data {
+	std::string	role;
+	std::string	name;
+	int			password;
+};
+
+#endif // !DATA_HPP
