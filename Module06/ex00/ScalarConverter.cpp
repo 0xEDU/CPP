2abe91791aba0ccdc1a90c2c2ebd98ed71c33e85
etachott <bfwaend@gmail.com>
2023-06-17 14:49:32 -0300

    feat: add simple validation for convert member function on ex00/m06

--- Module06/ex00/ScalarConverter.cpp
+++ Module06/ex00/ScalarConverter.cpp
@@ -6,12 +6,13 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 16:30:55 by edu               #+#    #+#             */
-/*   Updated: 2023/06/17 13:22:43 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/17 14:48:12 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include "formattedPrint.hpp"
+#include <cctype>
 
 ScalarConverter::ScalarConverter() {
 	return ;
@@ -31,11 +32,39 @@ ScalarConverter::~ScalarConverter() {
 	return ;
 }
 
+static bool isValidFloat(std::string str) {
+	int dotCounter = 0;
+
+	for (size_t i = 0; i < str.length() - 1; i++) {
+		if (!std::isdigit(str[i]) && str[i] != '.')
+			return false;
+		if (str[i] == '.')
+			dotCounter++;
+	}
+	if (str[str.length() - 1] == 'f' && std::isdigit(str[str.length() - 2]))
+		return true;
+	return false;
+}
+
+static bool isValid(std::string str) {
+	for (size_t i = 0; i < str.length(); i++) {
+		str[i] = std::tolower(str[i]);
+	}
+	if (str == "nan" || str == "-inf" || str == "+inf"
+		|| str == "-inff" || str == "+inff")
+		return true;
+	for (size_t i = 0; i < str.length(); i++) {
+		if (!std::isdigit(str[i]) && str[i] != '.' && !isValidFloat(str))
+			return false;
+	}
+	return true;
+}
+
 void ScalarConverter::convert(char *input) {
 	std::string str = input;
 
-	// if (parse(input) == ERROR)
-	// 	return ;
+	if (isValid(str) == false)
+		return ;
 	formattedPrintChar(str);
 	formattedPrintInt(str);
 	formattedPrintFloat(str);
