f4371d24d11853daf13a6c5a4c3042b60f035d5d
0xEDU <bfwaend@gmail.com>
2023-06-18 14:03:43 -0300

    style: add macros for colors on ex01/m06

--- Module06/ex01/Data.hpp
+++ Module06/ex01/Data.hpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 18:13:42 by etachott          #+#    #+#             */
-/*   Updated: 2023/06/17 18:15:02 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/18 13:56:46 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 #define DATA_HPP
 
 #include <iostream>
+
+#define YELLOW "\033[1;33m"
+#define END "\033[0m"
 
 struct Data {
 	std::string	role;
