c9cb334a13ff0cb97fb13c32668da7620cf34b07
etachott <bfwaend@gmail.com>
2023-06-20 14:54:19 -0300

    refactor: move includes from cpp to header file

--- Module06/ex00/formattedPrint.cpp
+++ Module06/ex00/formattedPrint.cpp
@@ -6,15 +6,11 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 16:56:41 by edu               #+#    #+#             */
-/*   Updated: 2023/06/20 14:39:19 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/20 14:53:13 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "formattedPrint.hpp"
-#include <cctype>
-#include <iomanip>
-#include <cmath>
-#include <limits>
 
 static bool isCharLiteral(std::string c) {
 	char upper = 'A';
@@ -30,7 +26,8 @@ static bool isCharLiteral(std::string c) {
 }
 
 static bool isSpecialString(std::string c) {
-	if (c == "nan" || c == "-inf" || c == "+inf"
+	if (c == "nan" || c == "nanf"
+		|| c == "-inf" || c == "+inf"
 		|| c == "-inff" || c == "+inff")
 		return true;
 	return false;
@@ -93,15 +90,18 @@ void formattedPrintFloat(std::string c) {
 void formattedPrintDouble(std::string c) {
 	std::cout << YELLOW << "double: " << END;
 	if (isCharLiteral(c) == true) {
-		std::cout << static_cast<float>(c[0]) << ".0" << std::endl;
+		std::cout << static_cast<double>(c[0]) << ".0" << std::endl;
 	} else {
 		double convertedDouble = std::strtod(c.c_str(), NULL);
-		long int convertedInt = std::atol(c.c_str());
+		float temp = convertedDouble;
+		int decimalPlaces = 0;
 
-		if (convertedDouble == convertedInt)
-			std::cout << static_cast<double>(convertedDouble)
-					<< std::endl;
-		else
-			std::cout << static_cast<double>(convertedDouble) << std::endl;
+		while (std::abs(temp - std::floor(temp)) > 0.0001) {
+			temp *= 10.0;
+			decimalPlaces++;
+		}
+		decimalPlaces == 0 ? decimalPlaces++ : decimalPlaces;
+		std::cout << std::fixed << std::setprecision(decimalPlaces)
+			<< static_cast<double>(convertedDouble) << std::endl;
 	}
 }
--- Module06/ex00/formattedPrint.hpp
+++ Module06/ex00/formattedPrint.hpp
@@ -6,15 +6,18 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 16:57:18 by edu               #+#    #+#             */
-/*   Updated: 2023/06/17 13:22:56 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/20 14:53:52 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FORMATTEDPRINT_HPP
 #define FORMATTEDPRINT_HPP
 
-#include <iostream>
+#include <cmath>
 #include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <limits>
 
 #define YELLOW "\033[1;33m"
 #define END "\033[0m"
