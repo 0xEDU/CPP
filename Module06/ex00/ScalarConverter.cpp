a884d01efdffec9fea8778718ed880f53e9cbb19
etachott <bfwaend@gmail.com>
2023-06-20 15:53:55 -0300

    feat: add function to get type on ex00/m06

--- Module06/ex00/ScalarConverter.cpp
+++ Module06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 16:30:55 by edu               #+#    #+#             */
-/*   Updated: 2023/06/20 14:47:57 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/20 15:52:35 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,13 +52,16 @@ static bool isValid(std::string str) {
 	for (size_t i = 0; i < str.length(); i++) {
 		str[i] = std::tolower(str[i]);
 	}
-	if (str.length() == 1 || str == "nan" || str == "nanf"
+	if ((str[0] == '-' && str.length() == 1) || str[0] == '.')
+		return false;
+	if ((str.length() == 1 && std::isalpha(str[0]))
+		|| str == "nan" || str == "nanf"
 		|| str == "-inf" || str == "+inf"
 		|| str == "-inff" || str == "+inff")
 		return true;
 	for (size_t i = 0; i < str.length(); i++) {
-		if (!std::isdigit(str[i]) && str[i] != '.' && !isValidFloat(str)
-				&& str[0] != '-')
+		if (!std::isdigit(str[i]) && !isValidFloat(str)
+				&& str[0] != '-' && str[i] != '.')
 			return false;
 	}
 	if (std::atol(str.c_str()) > std::numeric_limits<int>::max()
@@ -67,11 +70,26 @@ static bool isValid(std::string str) {
 	return true;
 }
 
+static std::string getType(std::string str) {
+	std::string type = "";
+
+	if (str[str.length() - 1] == 'f' && str.size() > 1)
+		return "float";
+	if (str.size() == 1 && std::isalpha(str[0]))
+		return "char";
+	if (str.find('.') != std::string::npos)
+		return "double";
+	return "int";
+}
+
 void ScalarConverter::convert(char *input) {
 	std::string str = input;
+	std::string type = "";
 
 	if (isValid(str) == false)
 		return ;
+	type = getType(str);
+	std::cout << type << std::endl;
 	formattedPrintChar(str);
 	formattedPrintInt(str);
 	formattedPrintFloat(str);
