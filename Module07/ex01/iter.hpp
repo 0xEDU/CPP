471b999184744df657c8900fb77b947c153e6254
etachott <bfwaend@gmail.com>
2023-06-23 14:27:27 -0300

    feat: add const version of Iter template function and elementPrint template function

--- Module07/ex01/iter.hpp
+++ Module07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 16:47:06 by edu               #+#    #+#             */
-/*   Updated: 2023/06/18 16:56:07 by edu              ###   ########.fr       */
+/*   Updated: 2023/06/23 14:26:01 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,33 @@
 #define ITER_HPP
 
 #include <cstddef>
+#include <iostream>
+
+template <typename T>
+void iter(T *arrayAddr, size_t size, void (*f)(const T &)) {
+	for (size_t i = 0; i < size; i++) {
+		f(arrayAddr[i]);
+	}
+	return ;
+};
 
 template <typename T>
 void iter(T *arrayAddr, size_t size, void (*f)(T &)) {
 	for (size_t i = 0; i < size; i++) {
 		f(arrayAddr[i]);
 	}
+	return ;
 };
 
 template <typename T>
 void elementDouble(T &element) {
 	element *= 2;
+	return ;
+};
+
+template <typename T>
+void elementPrint(T &element) {
+	std::cout << element;
+	return ;
 };
 #endif
