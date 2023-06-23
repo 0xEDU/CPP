54ff336aac0534726bcd24c3fe075f8bf244b6a3
etachott <bfwaend@gmail.com>
2023-06-23 18:13:03 -0300

    feat: add template function 'easyfind' on ex00/m08

---
+++ Module08/ex00/easyfind.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/23 17:59:00 by etachott          #+#    #+#             */
+/*   Updated: 2023/06/23 18:12:00 by etachott         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+
+template <typename T>
+T &easyfind(T &haystack, int needle) {
+	(void)needle;
+	return (haystack);
+};
+
+#endif // !EASYFIND_HPP
