bd7056e428b61ceb6ddbbeac51dc999f4d393f9e
etachott <bfwaend@gmail.com>
2023-06-24 20:17:58 -0300

    feat: add easyfind function implementation on ex00/m08

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -6,19 +6,23 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 17:59:00 by etachott          #+#    #+#             */
-/*   Updated: 2023/06/23 18:12:00 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/24 20:04:51 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-#include <iostream>
+#include <algorithm>
+#include <stdexcept>
 
 template <typename T>
-T &easyfind(T &haystack, int needle) {
-	(void)needle;
-	return (haystack);
+typename T::iterator easyfind(T &haystack, int needle) {
+	typename T::iterator it =
+		std::find(haystack.begin(), haystack.end(), needle);
+	if (*it != needle)
+		throw std::out_of_range("Value not found!");
+	return it;
 };
 
 #endif // !EASYFIND_HPP
