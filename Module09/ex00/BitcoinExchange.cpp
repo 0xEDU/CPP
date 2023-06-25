35de5d93b9dc57fdb968f9f74b828f2c28c5502a
etachott <bfwaend@gmail.com>
2023-06-25 10:07:14 -0300

    feat: add loadDB member function to BitcoinExchange class on ex00/m09

--- Module09/ex00/BitcoinExchange.cpp
+++ Module09/ex00/BitcoinExchange.cpp
@@ -6,21 +6,21 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/25 08:12:25 by etachott          #+#    #+#             */
-/*   Updated: 2023/06/25 08:25:13 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/25 10:05:18 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
 BitcoinExchange::BitcoinExchange()
-	: _dbMap(std::map<int, int>()),
-	_inputMap(std::map<int, int>()) {
+	: _dbMap(std::map<int, double>()),
+	_inputMap(std::map<int, double>()) {
 	return ;
 }
 
 BitcoinExchange::BitcoinExchange(const BitcoinExchange &rhs)
-	: _dbMap(std::map<int, int>()),
-	_inputMap(std::map<int, int>()) {
+	: _dbMap(std::map<int, double>()),
+	_inputMap(std::map<int, double>()) {
 	*this = rhs;
 	return ;
 }
@@ -32,5 +32,37 @@ BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &rhs) {
 }
 
 BitcoinExchange::~BitcoinExchange() {
+	return ;
+}
+
+const char *BitcoinExchange::DatabaseNotFound::what() const throw() {
+	return ("Error: Database not found!");
+}
+
+static const char *removeDash(std::string &str) {
+	str.erase(
+		std::remove(str.begin(), str.end(), '-'),
+		str.end()
+	);
+	return (str.c_str());
+}
+
+void BitcoinExchange::loadDB(const std::string db) {
+	std::ifstream input(db.c_str());
+	std::string line;
+
+	if (input.peek() == std::ifstream::traits_type::eof())
+		throw DatabaseNotFound();
+	while (std::getline(input, line)) {
+		std::string date = line.substr(0, line.find(","));
+		std::string value = line.substr(date.size() + 1, line.find(","));
+
+		this->_dbMap.insert(
+			std::make_pair(
+				std::atoi(removeDash(date)),
+				std::strtod(value.c_str(), NULL)
+			)
+		);
+	}
 	return ;
 }
