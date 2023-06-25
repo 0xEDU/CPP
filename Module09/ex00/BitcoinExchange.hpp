e7298553f6cadfe345b287cbdc8812e67df04741
etachott <bfwaend@gmail.com>
2023-06-25 10:08:44 -0300

    feat: add loadDB prototype and headers to BitcoinExchange class on ex00/m09

--- Module09/ex00/BitcoinExchange.hpp
+++ Module09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/25 04:03:22 by etachott          #+#    #+#             */
-/*   Updated: 2023/06/25 08:23:42 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/25 09:53:22 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,30 @@
 #define BITCOINEXCHANGE_HPP
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <exception>
 #include <map>
+#include <cstdlib>
+#include <utility>
+#include <algorithm>
 
 class BitcoinExchange {
 	private:
-		std::map<int, int> _dbMap;
-		std::map<int, int> _inputMap;
+		std::map<int, double> _dbMap;
+		std::map<int, double> _inputMap;
 
 	public:
 		BitcoinExchange();
 		BitcoinExchange(const BitcoinExchange &);
 		BitcoinExchange &operator=(const BitcoinExchange &);
 		~BitcoinExchange();
+
+		class DatabaseNotFound : public std::exception {
+			const char *what(void) const throw();
+		};
+
+		void	loadDB(const std::string db);
 };
 
 #endif // !BITCOINEXCHANGE_HPP
