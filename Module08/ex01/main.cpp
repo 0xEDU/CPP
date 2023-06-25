efcb2c2df59128691c684a6fca525f8c74e9d450
etachott <bfwaend@gmail.com>
2023-06-25 00:35:58 -0300

    tests: add addNumbers tests on ex01/m08

--- Module08/ex01/main.cpp
+++ Module08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 21:20:18 by etachott          #+#    #+#             */
-/*   Updated: 2023/06/24 23:55:13 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/25 00:33:51 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@
 
 #define YELLOW "\033[1;33m"
 #define END "\033[0m"
+
+std::ostream &operator<<(std::ostream &o, const Span sp) {
+	for (t_capacity i = 0; i < sp.getCurrentCapacity(); i++) {
+		o << sp.at(i) << " ";
+	}
+	return o;
+}
 
 int main() {
 	std::cout << YELLOW << "=== CREATING AN EMPTY SPAN ==="
@@ -35,15 +42,20 @@ int main() {
 		try {
 			Span sp = Span(10);
 
+			std::cout << "Size before adding multiple numbers: " <<
+				sp.getCurrentCapacity() << std::endl;
 			for (int i = 0; i < 10; i++) {
-				std::cout << "Adding: " << i << std::endl;
 				sp.addNumber(i);
 			}
+			std::cout << "Size after adding multiple numbers: " <<
+				sp.getCurrentCapacity() << std::endl;
+			std::cout << "Span content: " << sp << std::endl;
 		} catch (std::exception &e) {
 			std::cerr << e.what() << std::endl;
 		}
 	}
-	std::cout << YELLOW << "\n=== GOING OVER THE SPAN CAPACITY ==="
+	std::cout << YELLOW << "\n=== GOING OVER THE SPAN CAPACITY " \
+		"ADDING ONE NUMBER AT A TIME ==="
 		<< END << std::endl;
 	{
 		try {
@@ -53,6 +65,39 @@ int main() {
 				std::cout << "Adding: " << i << std::endl;
 				sp.addNumber(i);
 			}
+		} catch (std::exception &e) {
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	std::cout << YELLOW << "\n=== FILLING THE SPAN WITH ONE FUNCTION CALL ==="
+		<< END << std::endl;
+	{
+		try {
+			Span sp = Span(10000);
+			std::vector<int> v(10000, 42);
+
+			std::cout << "Size before adding multiple numbers: " <<
+				sp.getCurrentCapacity() << std::endl;
+			sp.addNumbers(v.begin(), v.end());
+			std::cout << "Size after adding multiple numbers: " <<
+				sp.getCurrentCapacity() << std::endl;
+		} catch (std::exception &e) {
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	std::cout << YELLOW << "\n=== GOING OVER THE SPAN CAPACITY " \
+		"WITH ONE FUNCTION CALL ==="
+		<< END << std::endl;
+	{
+		try {
+			Span sp = Span(10000);
+			std::vector<int> v(20000, 42);
+
+			std::cout << "Size before adding multiple numbers: " <<
+				sp.getCurrentCapacity() << std::endl;
+			sp.addNumbers(v.begin(), v.end());
+			std::cout << "Size after adding multiple numbers: " <<
+				sp.getCurrentCapacity() << std::endl;
 		} catch (std::exception &e) {
 			std::cerr << e.what() << std::endl;
 		}
