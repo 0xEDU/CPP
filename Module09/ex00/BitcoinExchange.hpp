d90fa74d2faa585f29f73fb5e693ea4acf13b777
etachott <bfwaend@gmail.com>
2023-06-25 07:54:50 -0300

    feat: add base structure for ex00/m09

---
+++ Module09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 04:03:22 by etachott          #+#    #+#             */
+/*   Updated: 2023/06/25 04:04:07 by etachott         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+
+#endif // !BITCOINEXCHANGE_HPP
