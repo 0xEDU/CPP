d364201f555e00ff739fea9e3d6ee6d174730469
etachott <bfwaend@gmail.com>
2023-06-24 23:09:41 -0300

    feat: add Span class OCF implementation on ex01/m08

---
+++ Module08/ex01/Span.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/24 23:02:26 by etachott          #+#    #+#             */
+/*   Updated: 2023/06/24 23:09:19 by etachott         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Span.hpp"
+
+// 	Variables:
+//
+// 	t_capacity		_N;
+// 	t_capacity		_currentCapacity;
+// 	std::vector<int>_content;
+//
+//	DONE:
+//
+// 	Span();
+// 	Span(const Span &);
+// 	Span &operator=(const Span &);
+// 	~Span();
+//
+// 	TODO:
+//
+// 	class SpanIsFull : public std::exception {
+// 		public:
+// 			const char *what(void) const throw();
+// 	};
+//
+// 	class SpanNotPossible : public std::exception {
+// 		public:
+// 			const char *what(void) const throw();
+// 	};
+//
+// 	Span(t_capacity N);
+//
+// 	void	addNumber(const int number);
+// 	void	addNumbers(
+// 		std::vector<int>::iterator begin,
+// 		std::vector<int>::iterator end,
+// 		const int number
+// 	);
+//
+// 	int	shortestSpan();
+// 	int	longestSpan();
+
+Span::Span() : _N(0), _currentCapacity(0), _content({0}) {
+	return ;
+}
+
+Span::Span(const Span &rhs)
+	: _N(rhs._N),
+	_currentCapacity(rhs._currentCapacity),
+	_content(rhs._content) {
+	*this = rhs;
+	return ;
+}
+
+Span &Span::operator=(const Span &rhs) {
+	this->_N = rhs._N;
+	this->_currentCapacity = rhs._currentCapacity;
+	this->_content = rhs._content;
+	return *this;
+}
+
+Span::~Span() {
+	return ;
+}
