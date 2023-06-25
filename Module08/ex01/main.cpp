c06e97d14725f1deb40dabb20dd028fdb8dc2835
etachott <bfwaend@gmail.com>
2023-06-24 23:38:35 -0300

    tests: add test for empty Span on ex01/m08

--- Module08/ex01/main.cpp
+++ Module08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 21:20:18 by etachott          #+#    #+#             */
-/*   Updated: 2023/06/24 21:23:40 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/24 23:30:26 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,29 @@
 #define END "\033[0m"
 
 int main() {
-	std::cout << YELLOW << "=== SUBJECT TESTS ===" << END << std::endl;
+	std::cout << YELLOW << "=== CREATING AN EMPTY SPAN ==="
+		<< END << std::endl;
 	{
-		Span sp = Span(5);
+		try {
+			Span sp = Span(10);
 
-		sp.addNumber(6);
-		sp.addNumber(3);
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(11);
-
-		std::cout << sp.shortestSpan() << std::endl;
-		std::cout << sp.longestSpan() << std::endl;
+			sp.shortestSpan();
+		} catch (std::exception &e) {
+			std::cerr << e.what() << std::endl;
+		}
 	}
+	// std::cout << YELLOW << "=== SUBJECT TESTS ===" << END << std::endl;
+	// {
+	// 	Span sp = Span(5);
+	//
+	// 	sp.addNumber(6);
+	// 	sp.addNumber(3);
+	// 	sp.addNumber(17);
+	// 	sp.addNumber(9);
+	// 	sp.addNumber(11);
+	//
+	// 	std::cout << sp.shortestSpan() << std::endl;
+	// 	std::cout << sp.longestSpan() << std::endl;
+	// }
 	return 0;
 }
