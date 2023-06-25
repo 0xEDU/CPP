cff7650c9f1bec0eae71e6284a5bd14d55982450
etachott <bfwaend@gmail.com>
2023-06-25 08:27:32 -0300

    feat: add BE class instantiation on main on ex00/m09

--- Module09/ex00/main.cpp
+++ Module09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/25 04:01:54 by etachott          #+#    #+#             */
-/*   Updated: 2023/06/25 07:54:10 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/25 08:27:50 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@ int main(int argc, char *argv[]) {
 	if (argc != 2) {
 		std::cout << "Usage: ./btc [file]" << std::endl;
 		return (1);
+	}
+	try {
+		BitcoinExchange be = BitcoinExchange();
+		// BitcoinExchange.loadDB("data.csv");
+		// BitcoinExchange.openFile(argv[1]);
+	} catch(std::exception &e) {
+		std::cerr << e.what() << std::endl;
 	}
 	return (0);
 }
