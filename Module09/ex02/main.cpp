5179b2c649417899c3b33643e01b0f01610c9142
etachott <bfwaend@gmail.com>
2023-06-27 16:58:23 -0300

    feat: add validation to main and debug messages on ex02/m09

--- Module09/ex02/main.cpp
+++ Module09/ex02/main.cpp
@@ -6,15 +6,24 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 19:12:59 by etachott          #+#    #+#             */
-/*   Updated: 2023/06/26 19:24:09 by etachott         ###   ########.fr       */
+/*   Updated: 2023/06/27 16:45:35 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
 int main(int argc, char *argv[]) {
-	PmergeMe pme;
-	(void) argc;
-	(void) argv;
+	try {
+		PmergeMe::validateInput(argc, argv + 1);
+
+		PmergeMe pme(argv);
+
+		std::cout << pme.getVector() << std::endl;
+		std::cout << pme.getList() << std::endl;
+		// pme.vMergeInsertionSort();
+		// pme.lMergeInsertionSort();
+	} catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
 	return (0);
 }
